28e5dd17e0ed9a559ce66273ece0bbe71d8c5d90
Muhammad Taha <tahasiddiqi89@gmail.com>
2021-11-07 17:44:10 +0500

    Added a bye bye line

--- greeetings.cpp
+++ greeetings.cpp
@@ -4,13 +4,8 @@ using namespace std;
 
 int main()
 {
-
     cout<<"Hello students. how are you doing today?";
     cout<<"Welcome to developer nation!";
-
-
-
-
-
+    cout<<"Bye bye";
     return 0;
 }
